812f1fd83261b6dcecbb7a7d50d812e7fb84a9c3
Shahriar Hasan <sh.shahriar5@gmail.com>
2023-06-26 12:30:36 +0600

    Screen created

---
+++ screens/startGameScreen.js
@@ -0,0 +1,13 @@
+import { TextInput,Button,View } from "react-native";
+
+function startGameScreen(){
+    return(
+        <View>
+            <TextInput
+                BUTTONS
+            />
+        </View>
+    );
+}
+
+export default startGameScreen;
